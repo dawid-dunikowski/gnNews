50079bb4c7912ffa878109d676ce117d3b21414b
dawid-dunikowski <dawdunik@gmailcom>
2023-04-19 23:28:54 +0200

    Clock code refractoring

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 const Timer = () => {
     const [value, setValue] = useState();
 
+    // The code imports the necessary dependencies React, useEffect, and useState.
     const time = () => {
       const time = new Date();
       const hour = time.getHours();
@@ -12,10 +13,14 @@ const Timer = () => {
       return `${hour} : ${min} : ${sec}`;
     }
 
+    // Using the useEffect hook, we set the value value to the current time every second.
     useEffect(() => {
       setValue(time());
+
+      // The setInterval() function allows code to execute every specified time (in this case, every 1000ms, or 1s).
       const interval = setInterval(() => setValue(time()), 1000);
   
+      // If the Timer component is unmounted, we finish executing the setInterval() function.
       return () => {
         clearInterval(interval);
       };
